8ad46c4b867375abb5e64bfd886fdfefae57d628
Александр Бабаскин <babaskin.alexandr@gmail.com>
2022-09-27 20:41:00 +0200

    Добавьте файлы проекта.

---
+++ Sloghnye uslovija/Program.cs
@@ -0,0 +1,158 @@
+﻿//Home_work_(Sloghnue_uslovija)_4
+
+
+
+
+
+
+
+
+
+//Home_work_(Sloghnue_uslovija)_5
+
+/*string[] products = GetDataProducts();
+float[] prices_float = GetDataPricesFloat();
+float[] sizes_float = GetDataSizesFloat();
+string[] prices_string = GetDataPricesString();
+string[] sizes_string = GetDataSizesString();
+
+Console.WriteLine("Enter min price:");
+float min_price = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter max price:");
+float max_price = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter min size:");
+float min_size = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter max size:");
+float max_size = float.Parse(Console.ReadLine());
+
+string[] filtered_array = Filter(min_price, max_price, min_size, max_size);
+
+foreach (string item in filtered_array)
+    Console.WriteLine(item);
+
+
+
+string[] Filter(float min_price, float max_price, float min_size, float max_size)
+{
+    string[] array_lines = GetData();
+
+    string[] array_Products = new string[Num(array_lines)];
+    int j = 0;
+
+    for (int i = 1; i < array_lines.Length; i++)
+    {
+        string[] array_fields = array_lines[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        if (!(float.Parse(array_fields[1]) >= min_price && float.Parse(array_fields[1]) <= max_price &&
+            float.Parse(array_fields[2]) >= min_size && float.Parse(array_fields[2]) <= max_size)) continue;
+        array_Products[j++] = array_fields[0];
+
+    }
+    return array_Products;
+}
+
+
+string[] GetDataSizesString()
+{
+    string[] array_lines = GetData();
+
+    string[] array_Sizes = new string[Num(array_lines)];
+    int j = 0;
+
+    for (int i = 1; i < array_lines.Length; i++)
+    {
+        string[] array_fields = array_lines[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        array_Sizes[j++] = array_fields[2];
+    }
+    return array_Sizes;
+}
+
+
+string[] GetDataPricesString()
+{
+    string[] array_lines = GetData();
+
+    string[] array_Prices = new string[Num(array_lines)];
+    int j = 0;
+
+    for (int i = 1; i < array_lines.Length; i++)
+    {
+        string[] array_fields = array_lines[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        array_Prices[j++] = array_fields[1];
+    }
+    return array_Prices;
+}
+
+
+float[] GetDataSizesFloat()
+{
+    string[] array_lines = GetData();
+
+    float[] array_Sizes = new float[Num(array_lines)];
+    int j = 0;
+
+    for (int i = 1; i < array_lines.Length; i++)
+    {
+        string[] array_fields = array_lines[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        array_Sizes[j++] = float.Parse(array_fields[2]);
+    }
+    return array_Sizes;
+}
+
+
+float[] GetDataPricesFloat()
+{
+    string[] array_lines = GetData();
+
+    float[] array_Prices = new float[Num(array_lines)];
+    int j = 0;
+
+    for (int i = 1; i < array_lines.Length; i++)
+    {
+        string[] array_fields = array_lines[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        array_Prices[j++] = float.Parse(array_fields[1]);
+    }
+    return array_Prices;
+}
+
+
+string[] GetDataProducts()
+{
+    string[] array_lines = GetData();
+
+    string[] array_Products = new string[Num(array_lines)];
+    int j = 0;
+
+    for (int i = 1; i < array_lines.Length; i++)
+    {
+        string[] array_fields = array_lines[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        array_Products[j++] = array_fields[0];  
+    }
+    return array_Products;
+}
+
+
+int Num(string[] array)
+{
+    int lines = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        string[] array_fields = array[i].Split(';');
+        if (array_fields.Length != 3) continue;
+        lines++;
+    }
+    return lines;
+}
+
+string[] GetData()
+{
+    string file = File.ReadAllText("D://Products.csv");
+    string[] array_lines = file.Split('\n');
+    return array_lines;
+}
+*/
